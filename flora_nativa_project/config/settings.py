a7c72b66ac6d8aac84cc135164e12c3f86ba970c
karenanco1 <fotoskarennanco@gmail.com>
2026-06-27 00:41:27 +0000

    host

--- flora_nativa_project/config/settings.py
+++ flora_nativa_project/config/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-%181yj-sq@h!)z*g+_a
 
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
